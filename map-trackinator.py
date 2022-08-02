3bac4117a2abc5f7135a41cfb668e40eef69c86e
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2022-08-01 21:31:17 -0400

    more map download nonsense

--- map-trackinator.py
+++ map-trackinator.py
@@ -40,12 +40,15 @@ import aiohttp.web
 
 PIL = import_maybe_installing_with_pip('PIL', 'Pillow')
 #import PIL.Image
-from PIL import Image
+from PIL import Image, ImageDraw
+
+xyzservices = import_maybe_installing_with_pip('xyzservices')
 
 #map_machine = import_maybe_installing_with_pip('map_machine', 'git+https://github.com/enzet/map-machine')
 #import map_machine.main # we only ever sub-process this out via map_machine.main.__file__
 
 contextily = import_maybe_installing_with_pip('contextily')
+import contextily
 
 
 # Globals
@@ -228,12 +231,23 @@ async def ws_req_handler(req):
 
   return ws
 
+def bound(value, _min, _max):
+  if value < _min:
+    return bound(value + (_max - _min), _min, _max)
+  if value > _max:
+    return bound(value - (_max - _min), _min, _max)
+  return value
+
+bad_xyz_i_values = [0,1,]
+
 async def http_map_req_handler(req):
+  global bad_xyz_i_values
+
   map_png = os.path.join('out', 'map.png')
   map_svg = os.path.join('out', 'map.svg')
 
   # If map is old, re-render
-  if not os.path.exists(map_png) or int(time.time()) - os.path.getmtime(map_png) > 4:
+  if not os.path.exists(map_png) or int(time.time()) - os.path.getmtime(map_png) > 60:
     pos_reps = get_pos_reps()
     min_lat = 999.0
     max_lat = -999.0
@@ -250,18 +264,115 @@ async def http_map_req_handler(req):
         max_lon = rep['lon']
     
     # Bound these a little
-    zoom_diff = 0.001
-    zoom_min = 1.0 - zoom_diff
-    zoom_max = 1.0 + zoom_diff
-    min_lat *= zoom_min if min_lat > 0 else zoom_max
-    max_lat *= zoom_max if max_lat > 0 else zoom_min
-    min_lon *= zoom_min if min_lon > 0 else zoom_max
-    max_lon *= zoom_max if max_lon > 0 else zoom_min
+    lat_scale = max_lat - min_lat
+    lon_scale = max_lon - min_lon
 
-    print(f'min_lat={min_lat} max_lat={max_lat} min_lon={min_lon} max_lon={max_lon}')
+    # EDIT not anymore. contextily borks if image too small, so force a min map size
+    # if lat_scale < 1.2:
+    #   lat_scale = 1.2
+    # if lon_scale < 1.2:
+    #   lon_scale = 1.2
+
+    zoom_out_fraction = 0.10
+    min_lat -= zoom_out_fraction * lat_scale
+    max_lat += zoom_out_fraction * lat_scale
+    min_lon -= zoom_out_fraction * lon_scale
+    max_lon += zoom_out_fraction * lon_scale
+
+    min_lat = bound(min_lat, -90.0, 90.0)
+    max_lat = bound(max_lat, -90.0, 90.0)
+    min_lon = bound(min_lon, -180.0, 180.0)
+    max_lon = bound(max_lon, -180.0, 180.0)
+
+    #zoom_diff = 0.0010
+    # zoom_diff = 0.0002
+    # zoom_min = 1.0 - zoom_diff
+    # zoom_max = 1.0 + zoom_diff
+    # min_lat *= zoom_min if min_lat > 0 else zoom_max
+    # max_lat *= zoom_max if max_lat > 0 else zoom_min
+    # min_lon *= zoom_min if min_lon > 0 else zoom_max
+    # max_lon *= zoom_max if max_lon > 0 else zoom_min
+
     
-    img, ext = contextily.bounds2img(min_lon, min_lat, max_lon, max_lat, ll=True)
+    print(f'min_lat={min_lat} max_lat={max_lat} min_lon={min_lon} max_lon={max_lon}')
+
+    # img, ext = (None, None)
+    # xyz_providers = [x for x in xyzservices.providers.values()]
+    # i = 0
+    # while img is None and i < len(xyz_providers):
+    #   try:
+    #     if not i in bad_xyz_i_values:
+    #       print(f'i={i}/{len(xyz_providers)}')
+    #       img, ext = contextily.bounds2img(
+    #         min_lon, # West
+    #         min_lat, # South
+    #         max_lon, # East
+    #         max_lat, # North
+    #         ll=True,
+    #         source=xyz_providers[i]
+    #         # source=xyzservices.TileProvider(
+    #         #   name='Google',
+    #         #   url='https://mt0.google.com/vt?x={x}&y={y}&z={z}',
+    #         #   attribution='(C) Google',
+    #         # )
+    #       )
+    #   except:
+    #     traceback.print_exc()
+    #     bad_xyz_i_values.append(i)
+    #     if len(bad_xyz_i_values) >= len(xyz_providers):
+    #       bad_xyz_i_values = []
+    #       i = 0
+    #       print('Reset bad_xyz_i_values!')
+    #       time.sleep(1)
+
+    #   i += 1
+
+    img, ext = contextily.bounds2img(
+      min_lon, # West
+      min_lat, # South
+      max_lon, # East
+      max_lat, # North
+      ll=True,
+      # source=xyzservices.TileProvider(
+      #   name='Google',
+      #   url='https://mt0.google.com/vt?x={x}&y={y}&z={z}',
+      #   attribution='(C) Google',
+      # )
+      source=xyzservices.TileProvider(
+        name='OSM',
+        url='https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        attribution='(C) OSM',
+      )
+    )
+
+
     img_o = Image.fromarray(img, 'RGBA')
+    img_w, img_h = img_o.size
+    
+    # Define lat,lon -> y,x translator so we can draw images & lines on top of things relative to the image
+    def lat_lon_2_xy(lat, lon):
+      return (
+        int(lon - min_lon) * (img_w / lon_scale),
+        int(lat - min_lat) * (img_h / lat_scale)
+      )
+
+    # Grap each name, pos_name_locations[x] is x's (lat, lon) locations from oldest -> newest
+    pos_name_locations = {}
+    for rep in pos_reps:
+      if not rep['name'] in pos_name_locations:
+        pos_name_locations[ rep['name'] ] = []
+      pos_name_locations[ rep['name'] ].append(
+        ( rep['lat'], rep['lon'] )
+      )
+
+    img_od = ImageDraw.Draw(img_o)
+    for name, coords in pos_name_locations.items():
+      img_od.line(
+        [lat_lon_2_xy(lat, lon) for lat,lon in coords],
+        width=1, fill=(250, 20, 20)
+      )
+
+
     img_o.save(map_png)
 
 
